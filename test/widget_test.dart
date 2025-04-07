c7c01313ed60034247063e3d53f3182539dfa234
Fahim-046 <fahimfardous8@gmail.com>
2025-04-07 12:25:59 +0600

    Add test for home screen.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,22 +8,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hello_flutter/presentation/app/app.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+class MockSupabaseClient extends Mock implements SupabaseClient {}
 
 void main() {
+  setUpAll(() async {
+    // Load environment variables for the test environment
+    await dotenv.load(fileName: '.env');
+    print(dotenv.env['SUPABASE_URL']);  // Ensure .env file is loaded
+    print(dotenv.env['SUPABASE_ANON_KEY']);
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Create mock Supabase client
+    final mockClient = MockSupabaseClient();
+
+    // Mock the Supabase client with the mock version
+    Supabase.instance.client = mockClient;
+
+    // Build the app and trigger a frame
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // Verify initial state of the app
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tap the '+' icon and trigger a frame
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
+    // Verify the counter increments
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
