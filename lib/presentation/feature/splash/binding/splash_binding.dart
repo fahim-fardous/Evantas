3472b58f6cdceee407b30116516852f39143442a
Fahim-046 <fahimfardous8@gmail.com>
2025-04-19 19:02:44 +0600

    Add navigation based on user logged in or not.

--- lib/presentation/feature/splash/binding/splash_binding.dart
+++ lib/presentation/feature/splash/binding/splash_binding.dart
@@ -1,4 +1,5 @@
 import 'package:domain/repository/app_repository.dart';
+import 'package:domain/repository/auth_repository.dart';
 import 'package:evntas/presentation/base/base_binding.dart';
 import 'package:evntas/presentation/feature/splash/splash_view_model.dart';
 
@@ -6,8 +7,10 @@ class SplashBinding extends BaseBinding {
   @override
   Future<void> addDependencies() async {
     AppRepository appRepository = await diModule.resolve<AppRepository>();
+    AuthRepository authRepository = await diModule.resolve<AuthRepository>();
     return diModule.registerInstance(
-      SplashViewModel(appRepository: appRepository),
+      SplashViewModel(
+        appRepository: appRepository, authRepository: authRepository,),
     );
   }
 
